0f99a950b37edbada14c5ea247a0ac88aca4de24
Todd Mariani <tmariani@reciprocalpress.com>
2013-11-01 17:00:57 -0700

    js committed

--- js/app.js
+++ js/app.js
@@ -1,113 +1,42 @@
 
 $(document).ready(function(){
 
-doubleSecretApiKey="kdnnwrs9su37a4mejftcfpun";
-doubleSecretSecret="9Wq9VF6XqB";
+
+			$('#genre_Action').click(function(e) {
+				// fix for firefox to get event click
+				var e = window.event || e;
+				var targ = e.target || e.srcElement;
+				//alert('load my action data');
+				displayMetaDeta();
+			});
 
 
-function genSig(apikey,secret) {
-    //var apikey = "kdnnwrs9su37a4mejftcfpun";
-    //var secret = "9Wq9VF6XqB";
-    var curdate = new Date();
-    var gmtstring = curdate.toGMTString();
-    var utc = Date.parse(gmtstring) / 1000;
-    return hex_md5(apikey + secret + utc);
+
+
+function displayMetaDeta(){
+
+//hide selections
+$('#selections').hide();
+$('#user_messages').hide();
+
+
+//show clues and answers
+$('#clue').show();
+$('#answer').show();
+
+
+$('#clue_label').html('cast info goes here');
+
+
+
+
 }
 
-didit=genSig(doubleSecretApiKey,doubleSecretSecret);
-
-
-console.log(didit);
-
-// initialize
-
-
-//split url to get correct server 
-	
-/*
-	var currenturl = $(location).attr('href');
-
-	var splitUrl=currenturl.split('://');
-	var scheme = splitUrl[0];
-	var serverName=splitUrl[1].split('/')[0];
-
-	myUrl = scheme + '://' + serverName + '';
-
-	*/	//make the call
-			$.ajax({				
-				 url: 'http://api.rovicorp.com/search/v2.1/video/search?entitytype=movie&query=hanks&rep=1&size=20&offset=0&language=en&country=US&format=json&apikey='+ doubleSecretApiKey + '&sig='+ didit,
-				 dataType: 'json',
-				 success: function( data ) {
-
-				 	console.log(data);
-
-				 	/*
-
-					 //add mso list to dropdown
-						$.each("data path goes here", function(index, value) {
-							$('#mylist').append(
-									'<option value="' + this.id + '">'
-									+ this.name + '</div>')
-						});
-
-						$('#mylist').show();*/
-						
-				 },
-				 error: function( data ) {
-					alert("Unfortunately, we have run into an issue, please contact the help desk");
-				 }
-				});
-
-			// check strings
-		/*	function validateStringCheck(string) {
-				if (string == "") {
-					string = "empty";
-				}
-				return string;
-			}
-
-
-*/
 
 				
 			});
 
 	
-
-//validate url and change element background according to validity
-
-function validateUrls(urlIn,idIn){
-	
-	if(urlIn!=""){
-	var url = urlIn;
-	url_validate = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-	if(!url_validate.test(url)){
-	   console.log('error');
-
-	   
-	}
-	else{
-	   console.log('success');
-
-	}
-	}
-	
-}
-
-
-
- //convert rgb to hex 
- // props to http://stackoverflow.com/questions/1740700/how-to-get-hex-color-value-rather-than-rgb-value
- function rgb2hex(rgb) {
-	    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-	    function hex(x) {
-	        return ("0" + parseInt(x).toString(16)).slice(-2);
-	    }
-	    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-	}
- 
-
-
 
 
 
