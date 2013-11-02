b0eefd8893888594ae3307e08c62d74a924a7d28
Todd Mariani <tmariani@reciprocalpress.com>
2013-11-02 06:47:51 -0700

    after choosing theme additions

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -1,5 +1,10 @@
-﻿$(document).ready(function () {
+$(document).ready(function () {
     
+
+    //auto-tab inputs
+    $(':input').autotab_magic();
+
+
      var keywordList = [];
      $.getJSON("app/stores/keywords.txt", function (data) {
 
@@ -37,6 +42,98 @@
     var movieList = [];
 
 
+    /*countdown timer stuff*/
+    function doneCounting(){
+    console.log("done counting");
+    $('#countdown').countdown('destroy');
+    }
+
+    function startCounting(){
+    now=new Date();
+    now.setSeconds(now.getSeconds() + 60);
+
+    $('#countdown').countdown({until: now, format: 'S',compact: true, onExpiry: doneCounting});
+    }
+    
+
+     /**** reset back to genre choice */
+    function resetToGenre () {
+
+        doneCounting();
+
+        //hide selections
+        $('#selections').show();
+        $('#user_messages').show();
+
+
+        //show clues and answers
+        $('#clue').hide();
+        $('#answer').hide();
+        $('#correct').hide();
+        $('#hints').hide();
+        $('#nav_button').hide();
+    }
+
+
+     /**** Show correct message and populate answer
+    function showCorrect(){
+    $('#correct').show();
+
+
+    $('#correct_label').html('You are Correct!');
+
+    }
+
+    /**** Check text as user inputs
+    $( "input" ).keyup(function() {
+         console.log("i changed value");
+
+       // checkAnswer();
+
+    });*/
+
+    /* display Movie info and hide genre choices*/
+
+        function displayMetaDeta(){
+
+
+    //grab the movie data
+    //moviename and cast list and hint  
+
+    //start counting
+    startCounting();
+    
+    //hide selections
+    $('#selections').hide();
+    $('#user_messages').hide();
+
+
+    //show clues and answers
+    $('#clue').show();
+    $('#answer').show();
+    $('#correct').show();
+    $('#hints').show();
+    $('#nav_button').show();
+
+    $('#hints').html('hint: my cast is...');
+    $('#nav_button').html('next');
+    $('#clue_label').html('cast info goes here');
+
+        $('#nav_button').click(function (e) {
+     // fix for firefox to get event click
+     var e = window.event || e;
+     var targ = e.target || e.srcElement;
+   
+        resetToGenre();
+    
+    });
+
+
+            }
+
+
+
+
      function getMovie(genre) {
 
          var apiUrl = 'http://brainbowweb.azurewebsites.net/api/Values?keyword=' + genre;
@@ -49,16 +146,27 @@
              },
              success: function(data) {
 
+                //display meta data
+
                  movieList = [];
 
                  $.each(data.searchResponse.results, function(key, val) {
-                     var queryStr = {
+                     queryStr = {
                          'MasterTitle': val.video.masterTitle,
                          'castUri': val.video.castUri
                      };
-                     alert('This is live title from API: ' + queryStr.MasterTitle);
+                     //alert('This is live title from API: ' + queryStr.MasterTitle);
                      movieList.push(queryStr);
                  });
+
+                 console.log(movieList);
+                    //display meta data
+
+                    displayMetaDeta();
+                    $('#answer').html(queryStr.MasterTitle);
+                    $('#clue_label').html(queryStr.castUri);
+
+
              }
          });
      }
